1d40e55774cd4e9866a47dcbe7aa5ec3216d4808
Aron Wussler <aron@wussler.it>
2021-05-26 14:46:04 +0200

    Some more stats

--- R/fairness.r
+++ R/fairness.r
@@ -4,17 +4,15 @@ require(data.table)
 # Load data
 df = read.csv('../locks.csv', header = FALSE, sep = ",", quote = "\"", dec = ".")
 colnames(df) = c("LockName", "NumThreads", "Count", "ThreadNum", "Time");
-df$Title = paste(df$LockName, "-", df$NumThreads, sep = " ")
+df$FullName = paste(df$LockName, "-", df$NumThreads, sep = " ")
 
-setDT(df)
-df[ , TimeDiff := Time - shift(Time), by = c("Title", "ThreadNum")]
-setDF(df)
+df = df[df$NumThreads==8,]
+fair = aggregate(df, list(df$FullName, df$ThreadNum), FUN=length)
 
-df = df[df$TimeDiff < 100, ]
-
-ggplot(df, aes(x=Title, y=TimeDiff)) + 
+ggplot(fair, aes(x=Group.1, y=NumThreads)) + 
   geom_boxplot(outlier.colour="black", outlier.shape=16, outlier.size=2) +
-  xlab("Lock") +
+  xlab("Lock")+
   theme(axis.text.x = element_text(angle = 90))
 
-ggsave("../report/fig/boxplot_all.pdf", height=4, width=10, dpi=1000)
+ggsave(paste("../report/fig/fairness_", "all", ".pdf", sep=""), height=8, width=12, dpi=1000)
+
